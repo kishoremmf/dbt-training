1ee32f505a3c92777b0f231284b74bd6a844bd2a
kishorem.mf@gmail.com <kishorem.mf@gmail.com>
2022-05-24 12:48:08 +0000

    developing stage models

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,22 @@
+select
+--from raw orders
+o.orderid,
+o.orderdate,
+o.shipdate,
+o.shipmode,
+o.ordersellingprice - o.ordercostprice as orderprofit,
+o.ordercostprice,
+o.ordersellingprice,
+-- from raw customer
+c.customername,
+c.segment,
+c.country,
+--from raw product
+p.category,
+-- p.productname,
+p.subcategory
+from {{ref('raw_order')}} o
+left join {{ref('raw_customer')}} c
+on o.customerid = c.customerid
+left join {{ref('raw_product')}} p 
+on o.productid = p.productid
