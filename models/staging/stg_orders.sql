b555e0ddda727a76e77b294090ef66a208195a30
kishorem.mf@gmail.com <kishorem.mf@gmail.com>
2022-05-26 14:29:03 +0000

    added seed file

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,5 +1,6 @@
 select
 --from raw orders
+{{ dbt_utils.surrogate_key(['o.orderid', 'c.customerid', 'p.productid']) }} as sk_orders,
 o.orderid,
 o.orderdate,
 o.shipdate,
@@ -16,9 +17,13 @@ c.customerid,
 p.category,
 p.subcategory2 as subcategory,
 p.subcategory as productname,
-p.productid
+p.productid,
+{{markup('ordersellingprice','ordercostprice')}} as markup,
+d.delivery_team
 from {{ref('raw_order')}} o
 left join {{ref('raw_customer')}} c
 on o.customerid = c.customerid
 left join {{ref('raw_product')}} p 
 on o.productid = p.productid
+left join {{ref('delivery_team')}} d
+on d.shipmode = o.shipmode
